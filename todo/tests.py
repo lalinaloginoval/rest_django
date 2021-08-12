15e7bccc49b715f847c77edf601a70df5875701f
lalinaloginoval <70517164+lalinaloginoval@users.noreply.github.com>
2021-08-12 19:03:16 +0300

    fix react project

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from mixer.backend.django import mixer
+
+from todo.models import Project
+from users.models import User
+
+
+class TestProjectViewSet(APITestCase):
+
+    def test_get_list(self):
+        response = self.client.get('/api/projects/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_mixer_project(self):
+        user = mixer.blend(User)
+        project = mixer.blend(Project, users=[user])
+        response = self.client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        project = mixer.blend(Project, name='popo')
+        response = self.client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_project = json.loads(response.content)
+        self.assertEqual(response_project['name'], 'popo')
